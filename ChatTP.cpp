fb7ff9340ef9d59b67984c8ad4474be82fbc6b47
mysteryon88 <68071039+mysteryon88@users.noreply.github.com>
2020-12-12 00:13:46 +0300

    Добавьте файлы проекта.

---
+++ ChatTP.cpp
@@ -0,0 +1,99 @@
+﻿
+// ChatTP.cpp: определяет поведение классов для приложения.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "ChatTP.h"
+#include "ChatTPDlg.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CChatTPApp
+
+BEGIN_MESSAGE_MAP(CChatTPApp, CWinApp)
+	ON_COMMAND(ID_HELP, &CWinApp::OnHelp)
+END_MESSAGE_MAP()
+
+
+// Создание CChatTPApp
+
+CChatTPApp::CChatTPApp()
+{
+	// поддержка диспетчера перезагрузки
+	m_dwRestartManagerSupportFlags = AFX_RESTART_MANAGER_SUPPORT_RESTART;
+
+	// TODO: добавьте код создания,
+	// Размещает весь важный код инициализации в InitInstance
+}
+
+
+// Единственный объект CChatTPApp
+
+CChatTPApp theApp;
+
+
+// Инициализация CChatTPApp
+
+BOOL CChatTPApp::InitInstance()
+{
+	// InitCommonControlsEx() требуется для Windows XP, если манифест
+	// приложения использует ComCtl32.dll версии 6 или более поздней версии для включения
+	// стилей отображения.  В противном случае будет возникать сбой при создании любого окна.
+	INITCOMMONCONTROLSEX InitCtrls;
+	InitCtrls.dwSize = sizeof(InitCtrls);
+	// Выберите этот параметр для включения всех общих классов управления, которые необходимо использовать
+	// в вашем приложении.
+	InitCtrls.dwICC = ICC_WIN95_CLASSES;
+	InitCommonControlsEx(&InitCtrls);
+
+	CWinApp::InitInstance();
+
+	if (!AfxSocketInit())
+	{
+		AfxMessageBox(IDP_SOCKETS_INIT_FAILED);
+		return FALSE;
+	}
+
+
+	AfxEnableControlContainer();
+
+	// Создать диспетчер оболочки, в случае, если диалоговое окно содержит
+	// представление дерева оболочки или какие-либо его элементы управления.
+	CShellManager *pShellManager = new CShellManager;
+
+	// Активация визуального диспетчера "Классический Windows" для включения элементов управления MFC
+	CMFCVisualManager::SetDefaultManager(RUNTIME_CLASS(CMFCVisualManagerWindows));
+
+	// Стандартная инициализация
+	// Если эти возможности не используются и необходимо уменьшить размер
+	// конечного исполняемого файла, необходимо удалить из следующих
+	// конкретных процедур инициализации, которые не требуются
+	// Измените раздел реестра, в котором хранятся параметры
+	// TODO: следует изменить эту строку на что-нибудь подходящее,
+	// например на название организации
+	SetRegistryKey(_T("Sergey Sobolev"));
+
+	CChatTPDlg dlg;
+	m_pMainWnd = &dlg;
+	INT_PTR nResponse = dlg.DoModal();
+	
+
+	// Удалить диспетчер оболочки, созданный выше.
+	if (pShellManager != nullptr)
+	{
+		delete pShellManager;
+	}
+
+#if !defined(_AFXDLL) && !defined(_AFX_NO_MFC_CONTROLS_IN_DIALOGS)
+	ControlBarCleanUp();
+#endif
+
+	// Поскольку диалоговое окно закрыто, возвратите значение FALSE, чтобы можно было выйти из
+	//  приложения вместо запуска генератора сообщений приложения.
+	return FALSE;
+}
+
---
+++ ChatTPDlg.h
@@ -0,0 +1,34 @@
+﻿
+// ChatTPDlg.h: файл заголовка
+//
+
+#pragma once
+
+
+// Диалоговое окно CChatTPDlg
+class CChatTPDlg : public CDialogEx
+{
+// Создание
+public:
+	CChatTPDlg(CWnd* pParent = nullptr);	// стандартный конструктор
+
+// Данные диалогового окна
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_CHATTP_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// поддержка DDX/DDV
+
+
+// Реализация
+protected:
+	HICON m_hIcon;
+
+	// Созданные функции схемы сообщений
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
